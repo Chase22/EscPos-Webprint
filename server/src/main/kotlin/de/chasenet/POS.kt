ad4446364175c69b7ad807c05618763e6dedf0ee
Chase22 <chase@mailbox.org>
2024-05-28 13:13:47 +0200

    Restart socket if it gets closed

--- server/src/main/kotlin/de/chasenet/POS.kt
+++ server/src/main/kotlin/de/chasenet/POS.kt
@@ -2,13 +2,20 @@ package de.chasenet
 
 import com.github.anastaciocintra.escpos.EscPos
 import com.github.anastaciocintra.escpos.Style
+import java.io.PipedOutputStream
 import java.net.Socket
 import java.nio.charset.Charset
 
-val pos = EscPos(Socket("192.168.1.133", 9000).getOutputStream())
-//val pos = EscPos(Socket("localhost", 1234).getOutputStream())
+private var socket = Socket("192.168.1.133", 9000)
+val pos = EscPos(socket.getOutputStream())
 
 fun printMessage(style: Style, message: String) {
+    if(socket.isOutputShutdown || socket.isClosed || !socket.isConnected) {
+        socket.close()
+        socket = Socket("192.168.1.133", 9000)
+        pos.outputStream = socket.getOutputStream()
+    }
+
     pos.writeLF(style, message)
     pos.feed(6)
     pos.cut(EscPos.CutMode.FULL)
